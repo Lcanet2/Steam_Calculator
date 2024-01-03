2ab9cb1f4c16feb5872a8197548527f7f4108bc5
Ower <canet.leo35@gmail.com>
2024-01-03 14:08:16 +0100

    font

--- src/components/Aboutcontainer.js
+++ src/components/Aboutcontainer.js
@@ -5,7 +5,6 @@ const Aboutcontainer = () => {
         <div className='text-container'>
             <h1>Quel futur pour CS : Stats ?</h1>
             <h2>Un futur radieu avec de nombreux axes :</h2>
-            <p>- Un back-end pour éviter d'installer l'extension.</p>
             <p>- S'étendre sur d'autres jeux que Counter Strike avec un système de liste déroulante.</p>
             <p>- Peut être encore + de statistiques si elles se voient intéressantes.</p>
             <h1>Pourquoi ai-je fais CS : Stats ?</h1>
--- src/components/Userdata.js
+++ src/components/Userdata.js
@@ -43,20 +43,21 @@ const UserData = ({ steamID }) => {
 
   return (
     <div>
-      <h1>Données de l'utilisateur :</h1>
+      <h1 className='titleData'>Données de l'utilisateur :</h1>
       {userData ? (
         <div className='Stat-Container'>
-          <h3>Total Kills : {formatStatValue(userData.playerstats.stats.find(stat => stat.name === 'total_kills')?.value)}</h3>
-          <h3>Total Morts : {formatStatValue(userData.playerstats.stats.find(stat => stat.name === 'total_deaths')?.value)}</h3>
-          <h3>Nombre d'heures en jeu : {totalTimePlayedInHours}</h3>
-          <h3>Total Wins: {formatStatValue(userData.playerstats.stats.find(stat => stat.name === 'total_wins')?.value)}</h3>
-          <h3>Nombre de bombes désamorcées : {formatStatValue(userData.playerstats.stats.find(stat => stat.name === 'total_defused_bombs')?.value)}</h3>
-          <h3>Nombre de bombes posées : {formatStatValue(userData.playerstats.stats.find(stat => stat.name === 'total_planted_bombs')?.value)}</h3>
-          <h3>Total Damage : {formatStatValue(userData.playerstats.stats.find(stat => stat.name === 'total_damage_done')?.value)}</h3>
-          <h3>Kills au couteau : {formatStatValue(userData.playerstats.stats.find(stat => stat.name === 'total_kills_knife')?.value)}</h3>
-          <h3>Nombre de balles tirées : {formatStatValue(userData.playerstats.stats.find(stat => stat.name === 'total_shots_fired')?.value)}</h3>
-          <h3>Nombre de balles touchées : {formatStatValue(userData.playerstats.stats.find(stat => stat.name === 'total_shots_hit')?.value)}</h3>
-          <h3>Nombre de kills contre un sniper qui vous vise : {formatStatValue(userData.playerstats.stats.find(stat => stat.name === 'total_kills_against_zoomed_sniper')?.value)}</h3>
+            <h3>Total Kills : <span className='numberApi'>{formatStatValue(userData.playerstats.stats.find(stat => stat.name === 'total_kills')?.value)}</span></h3>
+            <h3>Total Morts : <span className='numberApi'>{formatStatValue(userData.playerstats.stats.find(stat => stat.name === 'total_deaths')?.value)}</span></h3>
+            <h3>Nombre d'heures en jeu : <span className='numberApi'>{totalTimePlayedInHours}</span></h3>
+            <h3>Total Rounds Gagnés: <span className='numberApi'>{formatStatValue(userData.playerstats.stats.find(stat => stat.name === 'total_wins')?.value)}</span></h3>
+            <h3>Nombre de bombes désamorcées : <span className='numberApi'>{formatStatValue(userData.playerstats.stats.find(stat => stat.name === 'total_defused_bombs')?.value)}</span></h3>
+            <h3>Nombre de bombes posées : <span className='numberApi'>{formatStatValue(userData.playerstats.stats.find(stat => stat.name === 'total_planted_bombs')?.value)}</span></h3>
+            <h3>Total Damage : <span className='numberApi'>{formatStatValue(userData.playerstats.stats.find(stat => stat.name === 'total_damage_done')?.value)}</span></h3>
+            <h3>Kills au couteau : <span className='numberApi'>{formatStatValue(userData.playerstats.stats.find(stat => stat.name === 'total_kills_knife')?.value)}</span></h3>
+            <h3>Nombre de balles tirées : <span className='numberApi'>{formatStatValue(userData.playerstats.stats.find(stat => stat.name === 'total_shots_fired')?.value)}</span></h3>
+            <h3>Nombre de balles touchées : <span className='numberApi'>{formatStatValue(userData.playerstats.stats.find(stat => stat.name === 'total_shots_hit')?.value)}</span></h3>
+            <h3>Nombre de kills contre un sniper qui vous vise : <span className='numberApi'>{formatStatValue(userData.playerstats.stats.find(stat => stat.name === 'total_kills_against_zoomed_sniper')?.value)}</span></h3>
+
         </div>
       ) : (
         <div aria-label="Orange and tan hamster running in a metal wheel" role="img" class="wheel-and-hamster">
